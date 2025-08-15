7e67907d310160018b8bcfaf76a009ccfac22584
DeDeDeK <216604342+DeDeDek@users.noreply.github.com>
2025-08-15 09:46:21 -0600

    Add patch cap increase items and progressive stadium items

--- worlds/kirby_air_ride/KARData.py
+++ worlds/kirby_air_ride/KARData.py
@@ -72,6 +72,83 @@ class MemoryAddress(IntEnum):
     TOP_RIDE_CHECKLIST_BOX_FILLER_NUM = 0x8053685A
     TOP_RIDE_CHECKLIST_BOX_FILLER_LIST_LENGTH = 0x8053685B
 
+    # this is an address that starts at the last 3 bytes of a 4 byte word. for this address, we treat each byte as binary,
+    # and each bit is a stadium unlock. 0 = locked, 1 = unlocked.
+    # enumerating which bit unlocks which stadiums:
+    # byte 1 (starting at most significant bit):
+    # 1: vs. king dedede (stadium 24)
+    # 2: single race 9 (stadium 23)
+    # 3: single race 8 (stadium 22)
+    # 4: single race 7 (stadium 21)
+    # 5: single race 6 (stadium 20)
+    # 6: single race 5 (stadium 19)
+    # 7: single race 4 (stadium 18)
+    # 8: single race 3 (stadium 17)
+    # byte 2 (starting at most significant bit):
+    # 1: single race 2 (stadium 16)
+    # 2: single race 1 (stadium 15)
+    # 3: destruction derby 5 (stadium 14)
+    # 4: destruction derby 4 (stadium 13)
+    # 5: destruction derby 3 (stadium 12)
+    # 6: destruction derby 2 (stadium 11)
+    # 7: destruction derby 1 (stadium 10)
+    # 8: kirby melee 2 (stadium 9)
+    # byte 3 (starting at most significant bit):
+    # 1: kirby melee 1 (stadium 8)
+    # 2: high jump (stadium 7)
+    # 3: target flight (stadium 6)
+    # 4: air glider (stadium 5)
+    # 5: drag race 4 (stadium 4)
+    # 6: drag race 3 (stadium 3)
+    # 7: drag race 2 (stadium 2)
+    # 8: drag race 1 (stadium 1)
+    CITY_TRIAL_UNLOCKED_STADIUMS_ADDRESS = 0x80536EE9
+    # the same as above, but for each bit, whether it says "NEW" or not next to the stadium
+    CITY_TRIAL_UNLOCKED_STADIUMS_NEW_LABEL_ADDRESS = 0x80536EED
+
+    # this address is the decimal number of the stadium event, from 0-23, which will occur after City Trial.
+    # this is the stadium number in the list -1.
+    # this can be set to a negative value at the beginning of the city trial, which (for some values) selects a stage
+    # and prevents it from actually being unlocked after the stadium is finished. In this way, we can
+    # gate stadiums by always selecting one that's already been unlocked or in the case that none have been unlocked,
+    # select the same one each time until more are unlocked.
+    CITY_TRIAL_STADIUM_EVENT_ADDRESS = 0x80535F85
+    # -1: sky sands race 1 lap
+    # -2: fantasy meadows race 1 lap
+    # -3: sky sands race 1 lap
+    # -4: fantasy meadows race 1 lap
+    # -5: sky sands race 1 lap
+    # -6: fantasy meadows race 1 lap
+    # -7: beanstalk park race 1 lap
+    # -8: fantasy meadows race 1 lap
+    # -9: beanstalk park race 1 lap
+    # -10: fantasy meadows race 1 lap
+    # -11: drag race 1
+    # -12: fantasy meadows race 1 lap
+    # -13: drag race 1
+    # ...
+
+
+def convert_stadium_unlocks_bytes_to_bits(b: bytes) -> list[int]:
+    """
+    convert the bytes value to a list of integer bits
+    b is assumed to be 3 bytes from dolphin_memory_engine.read_bytes
+    """
+    integer_value = int.from_bytes(b, byteorder="big")
+    binary_string = f"{integer_value:024b}"
+    return [int(i) for i in binary_string]
+
+
+def compose_stadium_unlocks_number(bits: list[int]) -> int:
+    """
+    compose a 24-bit 3-byte hex number to write to memory with dolphin_memory_engine.write_bytes
+    bits is a list of 24 bits, least significant last.
+    """
+    int_val = 0
+    for i, bit in enumerate(reversed(bits)):  # process from LSB to MSB
+        int_val |= bit << i
+    return int_val
+
 
 class MenuSelectionID(IntEnum):
     AIR_RIDE = 0
@@ -123,9 +200,21 @@ class StageName(StrEnum):
     STADIUM_DESTRUCTION_DERBY_1 = "Stadium: DESTRUCTION DERBY 1"
     STADIUM_DESTRUCTION_DERBY_2 = "Stadium: DESTRUCTION DERBY 2"
     STADIUM_DESTRUCTION_DERBY_3 = "Stadium: DESTRUCTION DERBY 3"
+    STADIUM_DESTRUCTION_DERBY_4 = "Stadium: DESTRUCTION DERBY 4"
+    STADIUM_DESTRUCTION_DERBY_5 = "Stadium: DESTRUCTION DERBY 5"
     STADIUM_KIRBY_MELEE_1 = "Stadium: KIRBY MELEE 1"
     STADIUM_KIRBY_MELEE_2 = "Stadium: KIRBY MELEE 2"
+    STADIUM_SINGLE_RACE_1 = "Stadium: SINGLE RACE 1"
+    STADIUM_SINGLE_RACE_2 = "Stadium: SINGLE RACE 2"
+    STADIUM_SINGLE_RACE_3 = "Stadium: SINGLE RACE 3"
+    STADIUM_SINGLE_RACE_4 = "Stadium: SINGLE RACE 4"
+    STADIUM_SINGLE_RACE_5 = "Stadium: SINGLE RACE 5"
+    STADIUM_SINGLE_RACE_6 = "Stadium: SINGLE RACE 6"
+    STADIUM_SINGLE_RACE_7 = "Stadium: SINGLE RACE 7"
+    STADIUM_SINGLE_RACE_8 = "Stadium: SINGLE RACE 8"
+    STADIUM_SINGLE_RACE_9 = "Stadium: SINGLE RACE 9"
     STADIUM_VS_KING_DEDEDE = "Stadium: VS. KING DEDEDE"
+    FANTASY_MEADOWS = "FANTASY MEADOWS"
     CELESTIAL_VALLEY = "CELESTIAL VALLEY"
     SKY_SANDS = "SKY SANDS"
     FROZEN_HILLSIDE = "FROZEN HILLSIDE"
@@ -136,43 +225,173 @@ class StageName(StrEnum):
     NEBULA_BELT = "NEBULA BELT"
 
 
+class ProgressiveStadiumUnlockType(StrEnum):
+    STADIUM_DRAG_RACE_1 = "Unlock Stadium: DRAG RACE 1"
+    STADIUM_DRAG_RACE_2 = "Unlock Stadium: DRAG RACE 2"
+    STADIUM_DRAG_RACE_3 = "Unlock Stadium: DRAG RACE 3"
+    STADIUM_DRAG_RACE_4 = "Unlock Stadium: DRAG RACE 4"
+    STADIUM_HIGH_JUMP = "Unlock Stadium: HIGH JUMP"
+    STADIUM_TARGET_FLIGHT = "Unlock Stadium: TARGET FLIGHT"
+    STADIUM_AIR_GLIDER = "Unlock Stadium: AIR GLIDER"
+    STADIUM_DESTRUCTION_DERBY_1 = "Unlock Stadium: DESTRUCTION DERBY 1"
+    STADIUM_DESTRUCTION_DERBY_2 = "Unlock Stadium: DESTRUCTION DERBY 2"
+    STADIUM_DESTRUCTION_DERBY_3 = "Unlock Stadium: DESTRUCTION DERBY 3"
+    STADIUM_DESTRUCTION_DERBY_4 = "Unlock Stadium: DESTRUCTION DERBY 4"
+    STADIUM_DESTRUCTION_DERBY_5 = "Unlock Stadium: DESTRUCTION DERBY 5"
+    STADIUM_KIRBY_MELEE_1 = "Unlock Stadium: KIRBY MELEE 1"
+    STADIUM_KIRBY_MELEE_2 = "Unlock Stadium: KIRBY MELEE 2"
+    STADIUM_SINGLE_RACE_1 = "Unlock Stadium: SINGLE RACE 1"
+    STADIUM_SINGLE_RACE_2 = "Unlock Stadium: SINGLE RACE 2"
+    STADIUM_SINGLE_RACE_3 = "Unlock Stadium: SINGLE RACE 3"
+    STADIUM_SINGLE_RACE_4 = "Unlock Stadium: SINGLE RACE 4"
+    STADIUM_SINGLE_RACE_5 = "Unlock Stadium: SINGLE RACE 5"
+    STADIUM_SINGLE_RACE_6 = "Unlock Stadium: SINGLE RACE 6"
+    STADIUM_SINGLE_RACE_7 = "Unlock Stadium: SINGLE RACE 7"
+    STADIUM_SINGLE_RACE_8 = "Unlock Stadium: SINGLE RACE 8"
+    STADIUM_SINGLE_RACE_9 = "Unlock Stadium: SINGLE RACE 9"
+    STADIUM_VS_KING_DEDEDE = "Unlock Stadium: VS. KING DEDEDE"
+
+
+def get_progressive_stadium_unlock_type_from_item_name(item_name: str) -> ProgressiveStadiumUnlockType | None:
+    try:
+        return ProgressiveStadiumUnlockType(item_name)
+    except ValueError:
+        return None
+
+
+def get_stage_name_from_stadium_unlock_type(stadium: ProgressiveStadiumUnlockType) -> StageName:
+    return StageName[stadium.name]
+
+
+# a tuple of the stadiums, in order from most significant bit to least significant bit in a 24 bit binary number
+BIT_POSITION_TO_STADIUM_MAP = (
+    StageName.STADIUM_VS_KING_DEDEDE,
+    StageName.STADIUM_SINGLE_RACE_9,
+    StageName.STADIUM_SINGLE_RACE_8,
+    StageName.STADIUM_SINGLE_RACE_7,
+    StageName.STADIUM_SINGLE_RACE_6,
+    StageName.STADIUM_SINGLE_RACE_5,
+    StageName.STADIUM_SINGLE_RACE_4,
+    StageName.STADIUM_SINGLE_RACE_3,
+    StageName.STADIUM_SINGLE_RACE_2,
+    StageName.STADIUM_SINGLE_RACE_1,
+    StageName.STADIUM_DESTRUCTION_DERBY_5,
+    StageName.STADIUM_DESTRUCTION_DERBY_4,
+    StageName.STADIUM_DESTRUCTION_DERBY_3,
+    StageName.STADIUM_DESTRUCTION_DERBY_2,
+    StageName.STADIUM_DESTRUCTION_DERBY_1,
+    StageName.STADIUM_KIRBY_MELEE_2,
+    StageName.STADIUM_KIRBY_MELEE_1,
+    StageName.STADIUM_HIGH_JUMP,
+    StageName.STADIUM_TARGET_FLIGHT,
+    StageName.STADIUM_AIR_GLIDER,
+    StageName.STADIUM_DRAG_RACE_4,
+    StageName.STADIUM_DRAG_RACE_3,
+    StageName.STADIUM_DRAG_RACE_2,
+    StageName.STADIUM_DRAG_RACE_1,
+)
+
+
 class PatchType(StrEnum):
+    # Up types
     TURN_UP = "Turn Up"
-    TURN_UP_PERMANENT_PLUS_ONE = "Turn Up Permanent +1"
-    TURN_DOWN = "Turn Down"
     BOOST_UP = "Boost Up"
-    BOOST_UP_PERMANENT_PLUS_ONE = "Boost Up Permanent +1"
-    BOOST_DOWN = "Boost Down"
     CHARGE_UP = "Charge Up"
-    CHARGE_UP_PERMANENT_PLUS_ONE = "Charge Up Permanent +1"
-    CHARGE_DOWN = "Charge Down"
     DEFENSE_UP = "Defense Up"
-    DEFENSE_UP_PERMANENT_PLUS_ONE = "Defense Up Permanent +1"
-    DEFENSE_DOWN = "Defense Down"
     GLIDE_UP = "Glide Up"
-    GLIDE_UP_PERMANENT_PLUS_ONE = "Glide Up Permanent +1"
-    GLIDE_DOWN = "Glide Down"
     HP_UP = "HP Up"
-    HP_UP_PERMANENT_PLUS_ONE = "HP Up Permanent +1"
-    HP_DOWN = "HP Down"
     WEIGHT_UP = "Weight Up"
-    WEIGHT_UP_PERMANENT_PLUS_ONE = "Weight Up Permanent +1"
-    WEIGHT_DOWN = "Weight Down"
     OFFENSE_UP = "Offense Up"
-    OFFENSE_UP_PERMANENT_PLUS_ONE = "Offense Up Permanent +1"
-    OFFENSE_DOWN = "Offense Down"
     TOP_SPEED_UP = "Top Speed Up"
-    TOP_SPEED_UP_PERMANENT_PLUS_ONE = "Top Speed Up Permanent +1"
-    TOP_SPEED_DOWN = "Top Speed Down"
     ALL_UP = "All Up"
+
+    # Down types
+    TURN_DOWN = "Turn Down"
+    BOOST_DOWN = "Boost Down"
+    CHARGE_DOWN = "Charge Down"
+    DEFENSE_DOWN = "Defense Down"
+    GLIDE_DOWN = "Glide Down"
+    HP_DOWN = "HP Down"
+    WEIGHT_DOWN = "Weight Down"
+    OFFENSE_DOWN = "Offense Down"
+    TOP_SPEED_DOWN = "Top Speed Down"
     ALL_DOWN = "All Down"
 
+    # Permanent types
+    TURN_UP_PERMANENT_PLUS_ONE = "Turn Up Permanent +1"
+    BOOST_UP_PERMANENT_PLUS_ONE = "Boost Up Permanent +1"
+    CHARGE_UP_PERMANENT_PLUS_ONE = "Charge Up Permanent +1"
+    DEFENSE_UP_PERMANENT_PLUS_ONE = "Defense Up Permanent +1"
+    GLIDE_UP_PERMANENT_PLUS_ONE = "Glide Up Permanent +1"
+    HP_UP_PERMANENT_PLUS_ONE = "HP Up Permanent +1"
+    WEIGHT_UP_PERMANENT_PLUS_ONE = "Weight Up Permanent +1"
+    OFFENSE_UP_PERMANENT_PLUS_ONE = "Offense Up Permanent +1"
+    TOP_SPEED_UP_PERMANENT_PLUS_ONE = "Top Speed Up Permanent +1"
 
-def get_patch_type_from_item_name(item_name: str | None):
-    for patch_type in PatchType:
-        if patch_type.value == item_name:
-            return patch_type
+
+class PatchCapIncreaseType(StrEnum):
+    TURN_CAP_INCREASE = "Turn Cap Increase +1"
+    BOOST_CAP_INCREASE = "Boost Cap Increase +1"
+    CHARGE_CAP_INCREASE = "Charge Cap Increase +1"
+    DEFENSE_CAP_INCREASE = "Defense Cap Increase +1"
+    GLIDE_CAP_INCREASE = "Glide Cap Increase +1"
+    HP_CAP_INCREASE = "HP Cap Increase +1"
+    WEIGHT_CAP_INCREASE = "Weight Cap Increase +1"
+    OFFENSE_CAP_INCREASE = "Offense Cap Increase +1"
+    TOP_SPEED_CAP_INCREASE = "Top Speed Cap Increase +1"
+    ALL_CAP_INCREASE = "Patch Cap Increase +1"
+
+
+def get_patch_cap_increase_type_from_item_name(item_name: str) -> PatchCapIncreaseType | None:
+    """Get patch cap increase type from item name using the PatchCapIncreaseType enum."""
+    try:
+        return PatchCapIncreaseType(item_name)
+    except ValueError:
+        return None
+
+
+class StatType(StrEnum):
+    """Base stat types that correspond to unique memory addresses."""
+
+    TURN = "Turn"
+    BOOST = "Boost"
+    CHARGE = "Charge"
+    DEFENSE = "Defense"
+    GLIDE = "Glide"
+    HP = "HP"
+    WEIGHT = "Weight"
+    OFFENSE = "Offense"
+    TOP_SPEED = "Top Speed"
+
+
+def get_patch_type_from_item_name(item_name: str) -> PatchType | None:
+    """Get patch type from item name using the combined PatchType enum."""
+    try:
+        return PatchType(item_name)
+    except ValueError:
+        return None
+
+
+def patch_type_to_stat_type(patch_type: PatchType) -> StatType | None:
+    """Convert PatchType to its corresponding StatType"""
+    for stat_type in StatType:
+        if stat_type.value in patch_type.value:
+            return stat_type
     return None
+
+
+# Mapping from StatType to MemoryAddress for easy lookup
+STAT_TO_MEMORY_MAP: dict[StatType, MemoryAddress] = {
+    StatType.TURN: MemoryAddress.PLAYER_1_STAT_TURN_PATCH_AMOUNT,
+    StatType.BOOST: MemoryAddress.PLAYER_1_STAT_BOOST_PATCH_AMOUNT,
+    StatType.CHARGE: MemoryAddress.PLAYER_1_STAT_CHARGE_PATCH_AMOUNT,
+    StatType.DEFENSE: MemoryAddress.PLAYER_1_STAT_DEFENSE_PATCH_AMOUNT,
+    StatType.GLIDE: MemoryAddress.PLAYER_1_STAT_GLIDE_PATCH_AMOUNT,
+    StatType.HP: MemoryAddress.PLAYER_1_STAT_HP_PATCH_AMOUNT,
+    StatType.WEIGHT: MemoryAddress.PLAYER_1_STAT_WEIGHT_PATCH_AMOUNT,
+    StatType.OFFENSE: MemoryAddress.PLAYER_1_STAT_OFFENSE_PATCH_AMOUNT,
+    StatType.TOP_SPEED: MemoryAddress.PLAYER_1_STAT_TOP_SPEED_PATCH_AMOUNT,
+}
 
 
 class CheckboxFillerType(StrEnum):
@@ -181,11 +400,12 @@ class CheckboxFillerType(StrEnum):
     TOP_RIDE_CHECKBOX_FILLER = "Checkbox Filler (Top Ride)"
 
 
-def get_checkbox_filler_type_from_item_name(item_name: str | None):
-    for checkbox_filler_type in CheckboxFillerType:
-        if checkbox_filler_type.value == item_name:
-            return checkbox_filler_type
-    return None
+def get_checkbox_filler_type_from_item_name(item_name: str) -> CheckboxFillerType | None:
+    """Get checkbox filler type from item name using the CheckboxFillerType enum."""
+    try:
+        return CheckboxFillerType(item_name)
+    except ValueError:
+        return None
 
 
 class EffectType(StrEnum):
@@ -193,11 +413,12 @@ class EffectType(StrEnum):
     FULL_HEAL = "Full Heal"
 
 
-def get_effect_type_from_item_name(item_name: str | None):
-    for effect_type in EffectType:
-        if effect_type.value == item_name:
-            return effect_type
-    return None
+def get_effect_type_from_item_name(item_name: str) -> EffectType | None:
+    """Get effect type from item name using the EffectType enum."""
+    try:
+        return EffectType(item_name)
+    except ValueError:
+        return None
 
 
 class Stage(NamedTuple):
