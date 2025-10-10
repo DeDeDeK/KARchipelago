f7e42549af41613d89d8bca6b9be261da7134fc2
DeDeDeK <216604342+DeDeDek@users.noreply.github.com>
2025-10-10 10:52:18 -0600

    Update archipelago.json, add build-release script

---
+++ build-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python Launcher.py "Build APWorlds" -- "Kirby Air Ride"
+python Launcher.py "Generate Template Options"
+
+cp build/apworlds/kirby_air_ride.apworld ~/Downloads
+cp 'Players/Templates/Kirby Air Ride.yaml' ~/Downloads/kirby_air_ride.yaml
