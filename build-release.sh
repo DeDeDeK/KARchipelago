2d38980493e14026ce4c4e2b18b5df18d6d10519
DeDeDeK <216604342+DeDeDek@users.noreply.github.com>
2025-11-10 10:00:44 -0700

    Skip opening folder when generating template yamls from build-release script

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python Launcher.py "Build APWorlds" -- "Kirby Air Ride"
-python Launcher.py "Generate Template Options"
+python Launcher.py "Generate Template Options" -- --skip_open_folder
 
 cp build/apworlds/kirby_air_ride.apworld ~/Downloads
 cp 'Players/Templates/Kirby Air Ride.yaml' ~/Downloads/kirby_air_ride.yaml
